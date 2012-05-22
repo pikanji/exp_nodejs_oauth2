0aa3cd10f8089f549e1e2392ca47b464aaa38af3
Kanji Furuhashi <kanji.furuhashi@gree.co.jp>
2012-05-22 09:08:06 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+var sys=require('sys');
+var http = require('http');
+
+var server = http.createServer(
+    function(request, response) {
+	response.writeHead(200, {'Content-Type':'text/plain'});
+	response.write('Hello World!\n');
+	response.end();
+    }
+).listen(8124);
+
+sys.log('Server running at http://127.0.0.1:8124');
